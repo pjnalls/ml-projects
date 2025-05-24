40dfc4290579aa18c4eec8bbf3f47dc1fcf86ad3
pjnalls <42269808+pjnalls@users.noreply.github.com>
2025-05-24 07:19:53 -0500

    feat(vis): add method to visualize rnn guesses

---
+++ nlp_from_scratch/src/visualization/visualize.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+
+import src.preprocess.dataset as dataset
+import src.evaluate.evaluate as evaluate
+
+
+def visualize(confusion, classes):
+
+    # Set up plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # NumPy uses CPU here so we need to use a CPU version
+    cax = ax.matshow(confusion.cpu().numpy())
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticks(np.arange(len(classes)), labels=classes, rotation=90)
+    ax.set_yticks(np.arange(len(classes)), labels=classes)
+
+    # Force label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    # Set up title
+    plt.title("Languages the RNN Guessed Correctly")
+
+    plt.show()
+
+visualize(evaluate.confusion, classes=dataset.alldata.labels_uniq)
